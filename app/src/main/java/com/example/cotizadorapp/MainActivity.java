bbd384fcd15128b1208b423a5615b18e7c695344
Macover <macover1507@hotmail.com>
2022-03-21 16:23:47 -0600

    segunda vista de paquetes categorias y elegir subPaquete añadida

--- app/src/main/java/com/example/cotizadorapp/MainActivity.java
+++ app/src/main/java/com/example/cotizadorapp/MainActivity.java
@@ -2,30 +2,63 @@ package com.example.cotizadorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.google.android.material.navigation.NavigationBarView;
+
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-Spinner spinner1;
-ArrayList<String> spinnerList = new ArrayList<>();
-ArrayAdapter<String> spinnerAdapter;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner dropdown = findViewById(R.id.spinner1);
-        String[] items = new String[]{"1", "2", "3"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.item_main, items);
-        dropdown.setAdapter(adapter);
+        spinner = (Spinner) findViewById(R.id.spinnerCategoriasVista);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categorias_array,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+
+        int precio = 0;
+        String nombreCategoria;
+        String nombrePaquete;
+
+        switch (pos){
+            case 0:
+
+                break;
+            case 1:
+
+                break;
+            case 2:
+                break;
+
+            case 3:
+                break;
+        }
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+    public void finalizarApp(View vista){
+        finish();
     }
 }
+
 
 
