e6467445ac9a85c7f16fecd5810c4cb69abd7fff
Macover <macover1507@hotmail.com>
2022-03-20 20:31:38 -0600

    primera vista princiál

--- app/src/main/java/com/example/cotizadorapp/MainActivity.java
+++ app/src/main/java/com/example/cotizadorapp/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.cotizadorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+Spinner spinner1;
+ArrayList<String> spinnerList = new ArrayList<>();
+ArrayAdapter<String> spinnerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner dropdown = findViewById(R.id.spinner1);
+        String[] items = new String[]{"1", "2", "3"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.item_main, items);
+        dropdown.setAdapter(adapter);
     }
 }
+
+
